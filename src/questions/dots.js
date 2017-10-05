2a0ae17dc3490f9b89b67f83fc3ad6cc6720a784
Travis <travis@beeryardtech.com>
2017-10-04 23:15:16 -0400

    Re-did struct of questions

--- src/questions/dots.js
+++ src/questions/dots.js
@@ -1,45 +1,53 @@
+/*******************************************************************************
+ * vim: tabstop=4:shiftwidth=4:expandtab:
+ ******************************************************************************/
 'use strict';
 
+const _ = require('lodash');
 const fp = require('lodash/fp');
 const fs = require('fs-extra');
 const path = require('path');
-
 const normp = (p) => require('expand-tilde')(p);
 const dest = (l) => normp('~/tmp/moved/') + path.basename(l);
 
-exports.dots = {
-    'question': {
-        'choices': [
-            {
-                'checked': true,
-                'name': '~/.bashrc',
-                'value': {
-                    'linkName': normp('~/.bashrc'),
-                    'target': normp('~/Dropbox/repos/beeryardtech/dots/_bashrc.txt'),
-                },
+const handler = fp.each((answer) => {
+    if(fs.pathExistsSync(answer.linkName)) {
+        fs.moveSync(
+            answer.linkName,
+            dest(answer.linkName),
+            {'overwrite': true}
+        );
+    }
+    fs.ensureSymlinkSync(answer.target, answer.linkName);
+});
+
+const question = {
+    'choices': [
+        {
+            'checked': false,
+            'name': '~/.bashrc',
+            'value': {
+                'linkName': normp('~/.bashrc'),
+                'target': normp('~/Dropbox/repos/beeryardtech/dots/_bashrc.txt'),
             },
-            {
-                'checked': true,
-                'name': '~/.bash_ps1',
-                'value': {
-                    'linkName': normp('~/.bash_ps1'),
-                    'target': normp('~/Dropbox/repos/beeryardtech/dots/_bash_ps1.txt'),
-                },
+        },
+        {
+            'checked': false,
+            'name': '~/.bash_ps1',
+            'value': {
+                'linkName': normp('~/.bash_ps1'),
+                'target': normp('~/Dropbox/repos/beeryardtech/dots/_bash_ps1.txt'),
             },
-        ],
-        'message': 'Which file do you want to link in?',
-        'name': 'dots',
-        'type': 'checkbox',
-    },
-    'fn': fp.each((answer) => {
-        if(fs.pathExistsSync(answer.linkName)) {
-            fs.moveSync(
-                answer.linkName,
-                dest(answer.linkName),
-                {'overwrite': true}
-            );
-        }
-        fs.ensureSymlinkSync(answer.target, answer.linkName);
-    }),
+        },
+    ],
+    'message': 'Which file do you want to link in?',
     'name': 'dots',
+    'type': 'checkbox',
 };
+
+_.assign(exports, {
+    'questions': [ question ],
+    'handlers': {
+        'dots': handler,
+    },
+});
